b6438a94d22df7b352ec54474c274ea5c871847f
mzg <101003441+Ma2h1G@users.noreply.github.com>
2024-08-17 15:43:39 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,16 +1,25 @@
-#include<stdio.h>
+//#include<stdio.h>
+#include<iostream>
+#include<vector>
+using namespace std;
 #define N 10
-void maxmin(int arr[],int *pt1,int *pt2,int n);
+int maxmin(int *arr,int *pt1,int *pt2,int n);
 int main()
 {
 	int array[N]={10,7,19,29,4,0,7,35,-16,21},*p1,*p2,a,b;
 	p1=&a;p2=&b;
 	maxmin(array,p1,p2,N);
+	vector<int> test(15, 1);
 	printf("max=%d,min=%d\n",a,b);
+	int len = test.size();
+	for(int i = 1; i < len; ++i){
+	    test[i] += test[i - 1];
+	    cout << test[i] << endl;
+	}
 	return 0;
 }
 
-int maxmin(int arr[],int *pt1,int *pt2,int n)
+int maxmin(int *arr,int *pt1,int *pt2,int n)
 {
 	int i;
 	*pt1=arr[0];*pt2=arr[0];
